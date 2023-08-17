3b1a301aaa5c7a510d10f6f1f5cba89f63e9a1e5
Jonas Tobias Hopusch <git@jotoho.de>
2023-08-17 12:38:54 +0200

    Test repo settings

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#
+# SPDX-FileCopyrightText: 2023 Jonas Tobias Hopusch <git@jotoho.de>
+# SPDX-License-Identifier: AGPL-3.0-only
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
